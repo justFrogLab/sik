4ef4bf55c949ed391cfde3700bb9cb5387c57ecd
ChangSik Kim <106213484+justFrogLab@users.noreply.github.com>
2022-07-14 17:56:56 +0900

    0

---
+++ Uni/Assets/00.Script/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] Transform playerSlot;
+    [SerializeField] Transform enemySlot;
+
+    List<Transform> playerSlots;
+    List<Transform> enemySlots;
+
+    private void Awake()
+    {
+        playerSlots = new List<Transform>(playerSlot.GetComponentsInChildren<Transform>());
+        enemySlots = new List<Transform>(enemySlot.GetComponentsInChildren<Transform>());
+    }
+}
